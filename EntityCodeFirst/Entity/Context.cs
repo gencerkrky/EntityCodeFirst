1b7e2de5f0c95eef36a96ad6acf4b200166a57cf
gencerkarakaya <gencerkrky@gmail.com>
2022-10-22 19:58:12 +0300

    Add project files.

---
+++ EntityCodeFirst/Entity/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityCodeFirst.Entity
+{
+    class Context: DbContext
+    {
+        public DbSet<Urunler> Urunlers { get; set; }
+    }
+}
---
+++ EntityCodeFirst/Entity/Urunler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCodeFirst.Entity
+{
+    internal class Urunler
+    {
+        [Key]
+        public int UrunId { get; set; }
+        public string UrunAd { get; set; }
+        public string UrunMarka { get; set; }
+        public string UrunKategori { get; set; }
+        public int UrunStok { get; set; }
+    }
+}
